c7a3b2d1f38fd230774344179635c92a5ae8e5fb
antoinegreuzard <antoine@antoinegreuzard.fr>
2024-03-14 11:37:51 +0100

    fix: ESLint: Missing return type on function. (@typescript-eslint/explicit-function-return-type)

--- src/cipher.ts
+++ src/cipher.ts
@@ -32,7 +32,7 @@ export const substitutionCipher = (str: string, alphabet: string, decrypt: boole
 export const vigenereCipher = (str: string, key: string, decrypt: boolean = false): string => {
   let keyIndex = 0
   const keyLength = key.length
-  const shiftChar = (char: string, shift: number) => {
+  const shiftChar = (char: string, shift: number): string => {
     let code = char.charCodeAt(0)
     const base = code < 97 ? 65 : 97
     code = ((code - base + (decrypt ? -shift : shift) + 26) % 26) + base
